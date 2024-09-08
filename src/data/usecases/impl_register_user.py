4c17bc2c5e0d6c73df7d315ce1a980280e4b73c1
victorgabrieldeon <victordeonroyale@gmail.com>
2024-09-07 21:14:50 -0300

    feat(src/data/usecases): add CreateUserRepository dependency to ImplRegisterUserUseCase

--- src/data/usecases/impl_register_user.py
+++ src/data/usecases/impl_register_user.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from data.protocols.cryptography.hasher import Hasher
+from data.protocols.repositories.create_user_repository import CreateUserRepository
 from domain.models.user import User
 from domain.usecases.register_user_usecase import (
     RegisterUserUseCase,
@@ -10,10 +11,14 @@ from domain.validators.email_available_validator import EmailAvailableValidator
 
 class ImplRegisterUserUseCase(RegisterUserUseCase):
     def __init__(
-        self, email_avaiable_validator: EmailAvailableValidator, hasher: Hasher
+        self,
+        email_avaiable_validator: EmailAvailableValidator,
+        hasher: Hasher,
+        create_user_repository: CreateUserRepository,
     ) -> None:
         self.email_avaiable_validator = email_avaiable_validator
         self.hasher = hasher
+        self.create_user_repository = create_user_repository
 
     async def execute(
         self, params: RegisterUserUseCase.Params
@@ -29,5 +34,7 @@ class ImplRegisterUserUseCase(RegisterUserUseCase):
             hashed_password=hashed_password,
             is_active=True,
         )
+
+        user = await self.create_user_repository.create(user)
 
         return RegisterUserUseCase.Response(**user.model_dump())
