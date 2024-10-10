4524dc2f7d5c709cc631cfe2171e778cb4b8f45e
victorgabrieldeon <victordeonroyale@gmail.com>
2024-10-10 09:15:38 -0300

    refactor(src/domain/models/base.py): add default factory to field id in BaseEntity

--- src/domain/models/base.py
+++ src/domain/models/base.py
@@ -1,13 +1,20 @@
 import datetime
+import uuid
 
 from pydantic import BaseModel, Field
 
 
 class BaseEntity(BaseModel):
-    id: str
+    """
+    Class base for all entities in the domain layer.
+    """
+
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+
     created_at: datetime.datetime = Field(
         default_factory=lambda: datetime.datetime.now(datetime.UTC)
     )
+
     updated_at: datetime.datetime = Field(
         default_factory=lambda: datetime.datetime.now(datetime.UTC)
     )
